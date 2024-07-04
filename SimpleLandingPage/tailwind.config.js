00e41225230e0f86776da11ceb550f10f26e6665
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2024-07-04 12:17:10 +0530

    Simple Layout

---
+++ SimpleLandingPage/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      width:{
+        '124':'31rem',
+      },
+      colors:{
+        color1: '#545BE8'
+      }
+    },
+  },
+  plugins: [],
+}
+
